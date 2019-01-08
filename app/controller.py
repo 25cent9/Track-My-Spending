46cad29ebfb1535f37ba35348696400c259e7395
Innocent Niyibizi <25cent9@gmail.com>
2019-01-08 10:13:52 -0600

    Debugging

--- app/controller.py
+++ app/controller.py
@@ -1,11 +1,14 @@
 from app import app
-from flask import render_template, request, redirect, jsonify
+from flask import render_template, request
 from datetime import date
 from app.utils import send_data_to_sheet, get_categories
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html', current_year=date.today().year, categories=get_categories())
+    return render_template('index.html', current_year=date.today().year,
+                           categories=get_categories())
+
 
 @app.route('/submit_data', methods=['POST'])
 def submit_data():
